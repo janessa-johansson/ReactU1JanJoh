8ce9579990ae6538592d76016ca75d6514ae48d4
Unknown <nessijj@yahoo.se>
2019-03-26 11:04:54 +0100

    Initial commit

---
+++ src/WrapperComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WrapperComponent extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
